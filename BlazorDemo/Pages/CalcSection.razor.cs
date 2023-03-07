00be75e1714117aaca8d9c5716ec786141844dab
BaggerFast <riosha3@gmail.com>
2023-03-07 18:58:53 +0300

    ADD: CalcService prototype

---
+++ BlazorDemo/Pages/CalcSection.razor.cs
@@ -0,0 +1,11 @@
+﻿using BlazorDemo.Services;
+using DataCore.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorDemo.Pages;
+
+public partial class CalcSection: LayoutComponentBase
+{
+    [Inject] private CalcDataService? CalcService { get; set; }
+
+}
